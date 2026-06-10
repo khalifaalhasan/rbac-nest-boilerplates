d36f4315fcd64a674027cea52a31a0df7c8e13b8
Khalifa Al Hasan <khlfaalhsn5@gmail.com>
2026-06-11 02:51:56 +0700

    chore: add validation pipe and remove resource typo modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './modules/auth/auth.module';
 import { DatabaseModule } from './modules/database/database.module';
 import { ConfigModule } from '@nestjs/config';
-import { ReolesModule } from './modules/reoles/reoles.module';
+import { RolesModule } from './modules/roles/roles.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     envFilePath: './.env',
-  }), DatabaseModule, ReolesModule],
+  }), DatabaseModule, RolesModule, RolesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/reoles/dto/update-reole.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateReoleDto } from './create-reole.dto';
-
-export class UpdateReoleDto extends PartialType(CreateReoleDto) {}
